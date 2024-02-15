81b23873f5bf33087d83273db530fee3ec2f14fa
Mishka122 <r3m8oqwe@yandex.ru>
2024-02-15 18:53:53 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-document.querySelector('.block').addEventListener('click', () => {
+document.querySelector('.block').addEventListener('click', (e) => {
     console.log('hey')
+    e.currentTarget.style.display = 'none'
 })
