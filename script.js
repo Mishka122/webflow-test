595713d6d16a72ba0236e7ce4fd7fc361f9a6077
Mishka122 <r3m8oqwe@yandex.ru>
2024-02-21 15:06:13 +0300

    red

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.querySelector('.block').addEventListener('click', (e) => {
     console.log('hey')
-    e.currentTarget.style.backgroundColor = 'green'
+    e.currentTarget.style.backgroundColor = 'red'
 })
